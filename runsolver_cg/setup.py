b15f601af0a0f79f2b894bd3eacb8c97be52d343
AnasShahab <anas.shahab75@gmail.com>
2020-09-24 05:14:13 +0200

    Updated

--- runsolver_cg/setup.py
+++ runsolver_cg/setup.py
@@ -2,34 +2,52 @@ from cgroupspy import trees
 import os
 import subprocess
 
-user=os.getlogin() #name of the user
 
 t=trees.Tree()
+user=os.getlogin() #name of the user
 
-name_cg="test" #name of the cgroup
 
 
-#Creating cgroups for cpuset
-cset=t.get_node_by_path('/cpuset/')
-cset_cg=cset.create_cgroup(name_cg)
+class Login():
+    def __init__(self):
+        name_cg=input("Enter the name of the cgroup you want to create: ") #name of the cgroup
+        try:
+            #Creating cgroups for cpuset
+            cset=t.get_node_by_path('/cpuset/')
+            cset_cg=cset.create_cgroup(name_cg)
 
-#creating cgroups for cpu
-cpu=t.get_node_by_path('/cpu/')
-cpu_cg=cpu.create_cgroup(name_cg)
+            #creating cgroups for cpu
+            cpu=t.get_node_by_path('/cpu/')
+            cpu_cg=cpu.create_cgroup(name_cg)
 
-#creating cgroups for memory
-memory=t.get_node_by_path('/memory/')
-memory_cg=memory.create_cgroup(name_cg)
+            #creating cgroups for memory
+            memory=t.get_node_by_path('/memory/')
+            memory_cg=memory.create_cgroup(name_cg)
 
-#paths of the cgroups
-path_cpuset="/sys/fs/cgroup/cpuset/"+name_cg
-path_cpu="/sys/fs/cgroup/cpu/"+name_cg
-path_memory="/sys/fs/cgroup/memory/"+name_cg
+            print ("Cgroups created by the name of",name_cg)
 
-#giving permissions to current user
-cmd1=["sudo","chown","-R",user,path_cpuset]
-cmd2=["sudo","chown","-R",user,path_cpu]
-cmd3=["sudo","chown","-R",user,path_memory]
-subprocess.run(cmd1)
-subprocess.run(cmd2)
-subprocess.run(cmd3)
+        except:
+            print ("Cgroup",name_cg,"already exists")
+
+        #paths of the cgroups
+        path_cpuset="/sys/fs/cgroup/cpuset/"+name_cg
+        path_cpu="/sys/fs/cgroup/cpu/"+name_cg
+        path_memory="/sys/fs/cgroup/memory/"+name_cg
+
+        #giving permissions to current user
+        cmd1=["sudo","chown","-R",user,path_cpuset]
+        cmd2=["sudo","chown","-R",user,path_cpu]
+        cmd3=["sudo","chown","-R",user,path_memory]
+        subprocess.run(cmd1)
+        subprocess.run(cmd2)
+        subprocess.run(cmd3)
+
+if __name__=="__main__":
+    logObj=Login()
+
+
+
+
+
+
+   
