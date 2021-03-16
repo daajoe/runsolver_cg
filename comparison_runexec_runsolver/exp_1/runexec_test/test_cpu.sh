b882cde8cb40e1dcc0991eade25c061083682294
AnasShahab <anas.shahab75@gmail.com>
2021-03-16 03:13:38 +0100

    files added for exp_1

--- comparison_runexec_runsolver/exp_1/runexec_test/test_cpu.sh
+++ comparison_runexec_runsolver/exp_1/runexec_test/test_cpu.sh
@@ -2,6 +2,6 @@
 i=0
 while [ $i -le 49 ]
 do
-    runexec --timelimit 14400s python3 test_cpu.py > stdout_new_$i.txt 2>stderr_new_$i.txt
+    runexec --timelimit 14400s --output output/output_$i.log python3 test_cpu.py > output/stdout_$i.txt 2>output/stderr_$i.txt
     i=`expr $i + 1`
 done
