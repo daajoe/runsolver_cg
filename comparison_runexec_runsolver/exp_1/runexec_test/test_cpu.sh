1a825cba74a494afd4195b36b4d7cedc385234c5
AnasShahab <anas.shahab75@gmail.com>
2021-03-22 01:01:38 +0100

    Adding results of runexec

--- comparison_runexec_runsolver/exp_1/runexec_test/test_cpu.sh
+++ comparison_runexec_runsolver/exp_1/runexec_test/test_cpu.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-i=0
-while [ $i -le 49 ]
+i=11
+while [ $i -le 50 ]
 do
-    runexec --timelimit 14400s --output output/output_$i.log python3 test_cpu.py > output/stdout_$i.txt 2>output/stderr_$i.txt
+    runexec --timelimit 14400s python3 test_cpu.py > std_out/stdout_$i.txt 2>std_err/stderr_$i.txt
     i=`expr $i + 1`
     sleep 2
 done
