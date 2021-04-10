e9ab2ebdeb126a53878c7fe9b2db9faba4dd8d2e
AnasShahab <anas.shahab75@gmail.com>
2021-04-11 00:57:33 +0200

    Added results of exp_7 with runsolver

--- comparison_runexec_runsolver/exp_7/runsolver_test/ffmpeg_background.sh
+++ comparison_runexec_runsolver/exp_7/runsolver_test/ffmpeg_background.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-end=$((SECONDS+10000))
-
-while [ $SECONDS -lt $end ]
+#end=$((SECONDS+10000))
+while true
 do
-    ffmpeg -y -nostdin -i ../sampleVideo.mp4 ../sampleVideo_converted.avi > stdout/stdout_ffmpeg.log 2>stderr/stderr_ffmpeg.log
+    i=1
+    while [ $i -le 2 ]
+    do
+        ffmpeg -y -nostdin -i ../sampleVideo.mp4 ../sampleVideo_converted.avi > stdout/stdout_ffmpeg_$i.log 2>stderr/stderr_ffmpeg_$i.log
+        i=`expr $i + 1`
+    done
+    echo "ffmpeg ran $SECONDS seconds for `expr $i - 1` times"
+sleep 60
 done
-echo "ffmpeg ran $SECONDS"
